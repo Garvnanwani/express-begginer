a59522379df4dc5f5cb1c719269c52c33b8535ed
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-08-17 01:40:03 +0530

    all routes

---
+++ route/mainRoutes.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const Router = require('express').Router()
+const apiMiddleware = require('../middlewares/apiProduct')
+
+Router.get('/', (req, res) => {
+  res.render('index', { title: 'my Home' })
+})
+Router.get('/download', (req, res) => {
+  res.download(path.resolve(__dirname) + '/../views/about.ejs')
+})
+
+Router.get('/about', (req, res) => {
+  res.render('about', { title: 'my About' })
+})
+Router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'contact page' })
+})
+Router.get('/api/products', apiMiddleware, (req, res) => {
+  res.json([
+    { name: 'naaz', class: 11 },
+    { name: 'adnan', class: 4 },
+  ])
+})
+
+module.exports = Router
